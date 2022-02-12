69cb1368184a2a1e621efea5460c229d81bba9c8
Edwin Karimi <edumuriithi58@gmail.com>
2022-02-12 16:29:20 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Axios from "axios";
+import { useState } from "react";
 
 function App() {
+
+  const [joke, setJoke] = useState("")
+  const [response, setResponse] = useState("");
+
+  const getJoke = () => {
+    Axios.get("https://v2.jokeapi.dev/joke/Any?type=twopart")
+      .then((response) => {
+        setJoke(response.data.setup)
+        setResponse(response.data.delivery)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <><div className="App">
+      <button onClick={getJoke}>Click to request joke</button>
     </div>
+    <div className="jokes">
+        Setup: {joke} <br></br>
+        <br></br>
+        Delivery: {response}
+    </div></>
   );
 }
 
